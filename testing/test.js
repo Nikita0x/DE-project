076667fdd107531bcd34d0bb2f8991c9c2bf5048
Nikita0x <klirmio21@gmail.com>
2023-05-03 23:21:41 +0300

    Added modal, added local storage. Need to fix variables *brand* on refresh

--- testing/test.js
+++ testing/test.js
@@ -9,6 +9,9 @@ let categoryBtnIdGlobal = '';
 let selectedTemplateId = '';
 let selectedCategory = '';
 
+// brand
+let brand = 'Google';
+
 // containers
 const mainMenuContainer = document.querySelector('.main-menu-container');
 const categoriesCard = document.querySelector('.categories');
@@ -24,6 +27,7 @@ newLanguageBtn.addEventListener('click', () => {
   button.innerText = 'new btn';
   button.id = Date.now().toString();
   button.classList.add('btn-language');
+  button.classList.add('btn');
   mainMenuContainer.appendChild(button);
 
   //local storage logic
@@ -42,29 +46,6 @@ newLanguageBtn.addEventListener('click', () => {
   });
 });
 
-// logic to clear all buttons in Category menu and render those which exist and have a parent of a selectedLanguage button and addEventListeners to them
-function showCategoriesForLanguage(languageId) {
-  categoriesCard.style.display = 'block';
-  categoriesContainer.innerHTML = '';
-
-  //local storage logic
-  let categoriesLoaded =
-    JSON.parse(localStorage.getItem('categoriesJSON')) || [];
-  console.log(categoriesLoaded);
-  categoriesLoaded.forEach((item) => {
-    if (item.selectedLanguage === selectedLanguage) {
-      const button = document.createElement('button');
-      categoriesContainer.appendChild(button);
-      button.innerText = 'new btn';
-      button.id = item.id;
-      button.classList.add('btn-category');
-    } else {
-      return;
-    }
-  });
-  showTemplatesForCategory(categoryBtnIdGlobal);
-}
-
 // render languages from localStorage on load
 function renderLanguages() {
   let languagesLoaded = JSON.parse(localStorage.getItem('languagesJSON')) || [];
@@ -75,6 +56,7 @@ function renderLanguages() {
     button.innerText = item.name;
     button.id = item.id;
     button.classList.add('btn-language');
+    button.classList.add('btn');
     button.addEventListener('click', (e) => {
       const buttonId = e.currentTarget.id;
       selectedLanguage = buttonId;
@@ -97,6 +79,7 @@ function addCategoryBtn(selectedLanguage, categoryBtnId) {
   button.innerText = 'new btn';
   button.id = categoryBtnId;
   button.classList.add('btn-category');
+  button.classList.add('btn');
   categoriesContainer.appendChild(button);
 
   //local storage logic
@@ -116,6 +99,30 @@ function addCategoryBtn(selectedLanguage, categoryBtnId) {
     console.log(`Selected Category ID: ${selectedCategory}`);
   });
 }
+// logic to clear all buttons in Category menu and render those which exist and have a parent of a selectedLanguage button and addEventListeners to them
+function showCategoriesForLanguage(languageId) {
+  categoriesCard.style.display = 'block';
+  categoriesContainer.innerHTML = '';
+
+  //local storage logic
+  let categoriesLoaded =
+    JSON.parse(localStorage.getItem('categoriesJSON')) || [];
+  console.log(categoriesLoaded);
+  categoriesLoaded.forEach((item) => {
+    if (item.selectedLanguage === selectedLanguage) {
+      const button = document.createElement('button');
+      categoriesContainer.appendChild(button);
+      button.innerText = 'new btn';
+      button.id = item.id;
+      button.classList.add('btn-category');
+      button.classList.add('btn');
+    } else {
+      return;
+    }
+  });
+  showTemplatesForCategory(categoryBtnIdGlobal);
+}
+
 function showTemplatesForCategory(categoryBtnIdGlobal) {
   const buttons = document.querySelectorAll('.btn-category');
   buttons.forEach((button) => {
@@ -124,6 +131,139 @@ function showTemplatesForCategory(categoryBtnIdGlobal) {
       console.log(selectedCategory);
       renderTemplates(selectedCategory);
     });
+  });
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// modal
+const editModal = document.getElementById('editModal');
+//btn edit modal
+const btnEdit = document.querySelector('.btn-edit');
+btnEdit.addEventListener('click', (e) => {
+  editModal.classList.remove('closed');
+  editModal.style.display = 'block';
+  editModal.classList.add('active');
+  e.stopPropagation();
+});
+//
+//edit modal save button
+const btnEditSave = document.querySelector('.save');
+btnEditSave.addEventListener('click', saveTemplate);
+function saveTemplate() {
+  let mainTemplate = JSON.parse(localStorage.getItem('template')) || {};
+  mainTemplate = textAreaMainMenu.value;
+  let replacedText = mainTemplate.replace(/\*brand\*/g, brand);
+  localStorage.setItem('template', JSON.stringify(replacedText));
+  editModal.style.backgroundColor = 'rgba(0, 255, 0, 0.85)';
+  setTimeout(function () {
+    editModal.style.backgroundColor = 'rgb(242, 175, 175, 0.85)';
+    editModal.classList.add('closed');
+    console.log('SAVED!');
+    editModal.classList.remove('active');
+  }, 500);
+  setTimeout(function () {
+    editModal.style.display = 'none';
+  }, 700);
+}
+//
+//
+
+//
+//
+//
+//
+//
+//
+//edit modal close button
+const btnEditClose = document.querySelector('.close');
+btnEditClose.addEventListener('click', () => {
+  editModal.classList.remove('active');
+  editModal.classList.add('closed');
+  setTimeout(function () {
+    editModal.style.display = 'none';
+  }, 300);
+});
+//
+//
+// text area of the main menu modal
+const textAreaMainMenu = document.getElementById('textAreaMainMenu');
+textAreaMainMenu.value = JSON.parse(localStorage.getItem('template')) || {};
+let replacedText = textAreaMainMenu.value.replace(/\*brand\*/g, brand);
+localStorage.setItem('template', JSON.stringify(replacedText));
+//
+//
+//
+//
+//
+const emptyTemplateMainBtn = document.querySelector('.empty-template__main');
+emptyTemplateMainBtn.addEventListener('click', (e) => {
+  insertTemplate();
+});
+//
+function insertTemplate() {
+  let parsedTemplate = JSON.parse(localStorage.getItem('template')) || {};
+  LicaTextArea.value = parsedTemplate;
+  console.log('function is working');
+}
+//
+// text area of the DE Lica
+const LicaTextArea = document.getElementById('textarea');
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//-------------------------------------------
+// templates
+//add new template
+newTemplateBtn.addEventListener('click', () => {
+  selectedTemplateId = Date.now().toString();
+  addTemplateBtn(categoryBtnIdGlobal, selectedTemplateId);
+});
+function addTemplateBtn(categoryBtnIdGlobal, selectedTemplateId) {
+  // create and append a button
+  const button = document.createElement('button');
+  button.innerText = 'new template';
+  button.id = selectedTemplateId;
+  button.classList.add('btn-template');
+  button.classList.add('btn');
+  templatesContainer.appendChild(button);
+
+  //local storage logic
+  let templatesParsed = JSON.parse(localStorage.getItem('templatesJSON')) || [];
+  const newBtn = {
+    id: button.id,
+    name: 'new btn',
+    selectedCategory: selectedCategory,
+  };
+  templatesParsed.push(newBtn);
+  localStorage.setItem('templatesJSON', JSON.stringify(templatesParsed));
+  console.log('function to add new template worked');
+  button.addEventListener('click', (e) => {
+    console.log(`template ID: ${e.target.id}`);
+    console.log(`selected category: ${newBtn.selectedCategory}`);
   });
 }
 
@@ -141,7 +281,7 @@ function renderTemplates(selectedCategory) {
       button.innerText = 'new template';
       button.id = item.id;
       button.classList.add('btn-template');
-
+      button.classList.add('btn');
       button.addEventListener('click', (e) => {
         console.log(`template ID: ${e.target.id}`);
         console.log(`selected category: ${selectedCategory}`);
@@ -149,37 +289,6 @@ function renderTemplates(selectedCategory) {
     } else {
       return;
     }
-  });
-}
-
-//-------------------------------------------
-// templates
-//add new template
-newTemplateBtn.addEventListener('click', () => {
-  selectedTemplateId = Date.now().toString();
-  addTemplateBtn(categoryBtnIdGlobal, selectedTemplateId);
-});
-function addTemplateBtn(categoryBtnIdGlobal, selectedTemplateId) {
-  // create and append a button
-  const button = document.createElement('button');
-  button.innerText = 'new template';
-  button.id = selectedTemplateId;
-  button.classList.add('btn-template');
-  templatesContainer.appendChild(button);
-
-  //local storage logic
-  let templatesParsed = JSON.parse(localStorage.getItem('templatesJSON')) || [];
-  const newBtn = {
-    id: button.id,
-    name: 'new btn',
-    selectedCategory: selectedCategory,
-  };
-  templatesParsed.push(newBtn);
-  localStorage.setItem('templatesJSON', JSON.stringify(templatesParsed));
-  console.log('function to add new template worked');
-  button.addEventListener('click', (e) => {
-    console.log(`template ID: ${e.target.id}`);
-    console.log(`selected category: ${newBtn.selectedCategory}`);
   });
 }
 
